61e546600fba9cf701de724e2c3b8b86897a334e
masoodehghan <masood8204@gmail.com>
2022-08-29 15:52:17 +0430

    add users app

--- hotel_reservation/settings.py
+++ hotel_reservation/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -112,7 +114,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+AUTH_USER_MODEL = 'users.CustomUser'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
