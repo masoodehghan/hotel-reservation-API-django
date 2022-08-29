2e29b2782b88dd8aa0755415ef13e480a612b4d4
masoodehghan <masood8204@gmail.com>
2022-08-29 16:17:30 +0430

    remove guest and host models and add hotel app

--- users/models.py
+++ users/models.py
@@ -2,16 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class GuestManager(models.Manager):
-    def get_queryset(self):
-        return super().get_queryset().filter(role=CustomUser.Roles.GUEST)
-
-
-class HostManager(models.Manager):
-    def get_queryset(self):
-        return super().get_queryset().filter(role=CustomUser.Roles.HOST)
-
-
 class CustomUser(AbstractUser):
     class Roles(models.TextChoices):
         HOST = 'h'
@@ -20,21 +10,3 @@ class CustomUser(AbstractUser):
 
     base_role = Roles.ADMIN
     role = models.CharField(max_length=1, choices=Roles.choices, default=base_role)
-
-
-class Guest(CustomUser):
-
-    base_role = super().Roles.GUEST
-    objects = GuestManager()
-
-    class Meta:
-        proxy = True
-
-
-class Host(CustomUser):
-
-    base_role = super().Roles.HOST
-    objects = HostManager()
-
-    class Meta:
-        proxy = True
